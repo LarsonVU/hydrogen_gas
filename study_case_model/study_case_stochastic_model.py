1dcc5926f9206faab68eabdbe29ecf64f9e8668b
LarsonVU <l.m.j.beemster@student.vu.nl>
2026-03-17 16:29:14 +0100

    helldos

--- study_case_model/study_case_stochastic_model.py
+++ study_case_model/study_case_stochastic_model.py
@@ -19,10 +19,10 @@ def safe_filename(s):
 FOLDER = "study_case_model/figures/main_run/"
 
 NUMBER_OF_STAGES = 3
-BRANCHES_PER_STAGE = {1: 1, 2: 4, 3: 4}
+BRANCHES_PER_STAGE = {1: 1, 2: 2, 3: 2}
 ALLOWED_DEVIATION = 0  # x% deviation from nominal values for scenarios
 
-NUMBER_OF_DENSITY_BOUNDS = 5
+NUMBER_OF_DENSITY_BOUNDS = 1
 RHO_LOW = 0.55
 RHO_HIGH = 0.70
 
