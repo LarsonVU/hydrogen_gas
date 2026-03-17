0dd3a56317bd46063580bc47346cdae4b68ba58d
LarsonVU <l.m.j.beemster@student.vu.nl>
2026-03-17 16:59:43 +0100

    dont you

--- study_case_model/Experiments/run_subsidy_experiment.sh
+++ study_case_model/Experiments/run_subsidy_experiment.sh
@@ -12,6 +12,14 @@ module load Gurobi/12.0.3-GCCcore-14.2.0
 # --- 2. Activate Virtual Environment ---
 source ~/hydrogen_venv/bin/activate
 
+
+# --- Verification Check ---
+echo "--- Environment Check ---"
+which python3               # Should point to ~/hydrogen_venv/bin/python3
+python3 --version           # Should be 3.13.5
+pip list | grep -E "numpy|pyomo|gurobipy" # Verify your key libraries are present
+echo "--------------------------"
+
 # --- 3. Dynamic Path Branching ---
 # Ensures we don't overwrite different git branch results
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
